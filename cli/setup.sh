e813a73d6cddd258d0799b809ecaf748add169f1
kavsrd13 <114801393+kavsrd13@users.noreply.github.com>
2025-09-03 19:54:33 +0530

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 
 # <az_ml_install>
 az extension add -n ml -y
@@ -9,6 +9,7 @@ az extension add -n ml -y
 GROUP="rg-dp100-labs"
 LOCATION="swedencentral"
 WORKSPACE="azuremlopsresources"
+SUBSCRIPTION_ID="42848f6c-f0f7-4143-b9d3-76441ceeda99"                 
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
@@ -26,3 +27,10 @@ then
 else
     echo "Workflows are using the new subscription."
 fi
+
+
+# <export_env_for_sdk>
+export AZURE_SUBSCRIPTION_ID=$SUBSCRIPTION_ID
+export AZURE_RESOURCE_GROUP=$GROUP
+export AZURE_WORKSPACE_NAME=$WORKSPACE
+# </export_env_for_sdk>
